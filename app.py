eda50c517f0c882febdc3dfbfff7738680ec76fe
srnivya <nivyasr29@gmail.com>
2026-02-02 00:16:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-def add(a, b):
-    return a + b
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "🚀 CI/CD App is running successfully!"
 
 if __name__ == "__main__":
-    print("Hello CI/CD Pipeline")
-    
+    app.run(host="0.0.0.0", port=5000)
